963f9c7f556c25b1d269a777fd21945d5e475c76
Johannes Demissie <demissie.johannes@gmail.com>
2022-10-16 16:50:49 +0200

    Fix listview scrolling to be precise.

--- lib/passage_page.dart
+++ lib/passage_page.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:scroll_to_index/scroll_to_index.dart';
 import "side_menu.dart";
 import 'extension/string_extension.dart';
 
@@ -32,7 +34,8 @@ class PassagePage extends StatefulWidget {
 }
 
 class _PassagePageState extends State<PassagePage> {
-  ScrollController itemController = ScrollController();
+  final scrollDirection = Axis.vertical;
+  late AutoScrollController itemController;
 
   _onPageViewChange(int page) {
     print("Current Page: " + page.toString());
@@ -44,32 +47,25 @@ class _PassagePageState extends State<PassagePage> {
     widget.verse = 1;
   }
 
-  Future scroolToVerse() async {
-    if (widget.verse > 2) {
-      double verseMaxScroll = itemController.position.maxScrollExtent;
-      double versePosition = verseMaxScroll / widget.verseSum * widget.verse;
-      await itemController.animateTo(versePosition,
-          duration: Duration(milliseconds: 800), curve: Curves.ease);
-      scroolToVerse2();
-    } else {
-      null;
+  Future scroolToIndex() async {
+    if (widget.verse > 1) {
+      await itemController.scrollToIndex(
+        widget.verse,
+        preferPosition: AutoScrollPosition.begin,
+      );
     }
-  }
-
-  Future scroolToVerse2() async {
-    double verseMaxScroll = itemController.position.maxScrollExtent;
-    double versePosition = verseMaxScroll / widget.verseSum * widget.verse;
-    // itemController.jumpTo(versePosition);
-    await itemController.animateTo(versePosition,
-        duration: Duration(milliseconds: 200), curve: Curves.ease);
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    itemController = AutoScrollController(
+        viewportBoundaryGetter: () =>
+            Rect.fromLTRB(0, 0, 0, MediaQuery.of(context).padding.bottom),
+        axis: scrollDirection);
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      scroolToVerse();
+      scroolToIndex();
     });
   }
 
@@ -93,94 +89,117 @@ class _PassagePageState extends State<PassagePage> {
           ),
           onPageChanged: _onPageViewChange,
           children: [
-            for (var chap = 1; chap <= widget.chapterSum; chap++)
+            for (int chap = 1; chap <= widget.chapterSum; chap++)
               ListView.builder(
+                  // To remember scroll position
+                  key: PageStorageKey(chap.toString()),
                   itemCount: bibleCurrent["$chap"].length,
+                  scrollDirection: scrollDirection,
                   controller: itemController,
                   itemBuilder: (context, index) {
                     if (bibleCurrent["$chap"][index]["num"] == "Title") {
                       if ('$chap. fejezet' !=
                           '${bibleCurrent["$chap"][index]["text_hu"]}') {
-                        return Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            '$chap. fejezet\n${bibleCurrent["$chap"][index]["text_hu"]}',
-                            style: Theme.of(context).textTheme.headline5,
-                            textAlign: TextAlign.center,
+                        return AutoScrollTag(
+                          key: ValueKey(index),
+                          controller: itemController,
+                          index: index,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              '$chap. fejezet\n${bibleCurrent["$chap"][index]["text_hu"]}',
+                              style: Theme.of(context).textTheme.headline5,
+                              textAlign: TextAlign.center,
+                            ),
                           ),
                         );
                       } else {
-                        return Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            '$chap. fejezet',
-                            style: Theme.of(context).textTheme.headline5,
-                            textAlign: TextAlign.center,
+                        return AutoScrollTag(
+                          key: ValueKey(index),
+                          controller: itemController,
+                          index: index,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              '$chap. fejezet',
+                              style: Theme.of(context).textTheme.headline5,
+                              textAlign: TextAlign.center,
+                            ),
                           ),
                         );
                       }
                     } else if (bibleCurrent["$chap"][index]["num"] ==
                         "Subtitle") {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          bibleCurrent["$chap"][index]["text_hu"],
-                          style: Theme.of(context).textTheme.headline5,
-                          textAlign: TextAlign.center,
+                      return AutoScrollTag(
+                        key: ValueKey(index),
+                        controller: itemController,
+                        index: index,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            bibleCurrent["$chap"][index]["text_hu"],
+                            style: Theme.of(context).textTheme.headline5,
+                            textAlign: TextAlign.center,
+                          ),
                         ),
                       );
                     } else {
-                      return Padding(
-                        padding: const EdgeInsets.all(0.0),
-                        child: Stack(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 30.0,
-                                  right: 30.0,
-                                  top: 8.0,
-                                  bottom: 8.0),
-                              child: RichText(
-                                text: TextSpan(children: [
-                                  TextSpan(
-                                    text:
-                                        "${bibleCurrent["$chap"][index]["num"]}",
-                                    style: TextStyle(
-                                      color: Colors.red,
-                                      fontSize: 15,
+                      return AutoScrollTag(
+                        key: ValueKey(index),
+                        controller: itemController,
+                        index: index,
+                        child: Padding(
+                          padding: const EdgeInsets.all(0.0),
+                          child: Stack(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 30.0,
+                                    right: 30.0,
+                                    top: 8.0,
+                                    bottom: 8.0),
+                                child: RichText(
+                                  text: TextSpan(children: [
+                                    TextSpan(
+                                      text:
+                                          "${bibleCurrent["$chap"][index]["num"]}",
+                                      style: TextStyle(
+                                        color: Colors.red,
+                                        fontSize: 15,
+                                      ),
                                     ),
-                                  ),
-                                  TextSpan(
-                                    text:
-                                        "${bibleCurrent["$chap"][index]["text_hu"]}"
-                                            .capitalizeFirst(),
-                                    style:
-                                        Theme.of(context).textTheme.bodyText1,
-                                  ),
-                                ]),
-                              ),
-                            ),
-                            if (bibleCurrent["$chap"][index]["ref"] != null)
-                              Positioned(
-                                right: 0.0,
-                                bottom: 0.0,
-                                child: SizedBox(
-                                  width: 38,
-                                  height: 38,
-                                  child: IconButton(
-                                    onPressed: () {
-                                      print(
-                                          "${bibleCurrent["$chap"][index]["ref"]}");
-                                    },
-                                    icon: Icon(
-                                      Icons.library_books,
-                                      color: Colors.red,
-                                      size: 20,
+                                    TextSpan(
+                                      text:
+                                          "${bibleCurrent["$chap"][index]["text_hu"]}"
+                                              .capitalizeFirst(),
+                                      style:
+                                          Theme.of(context).textTheme.bodyText1,
                                     ),
-                                  ),
+                                  ]),
                                 ),
-                              )
-                          ],
+                              ),
+                              if (bibleCurrent["$chap"][index]["ref"] != null)
+                                Positioned(
+                                  right: 0.0,
+                                  bottom: 0.0,
+                                  child: SizedBox(
+                                    width: 38,
+                                    height: 38,
+                                    child: IconButton(
+                                      onPressed: () {
+                                        print(
+                                            "${bibleCurrent["$chap"][index]["ref"]}");
+                                      },
+                                      icon: Icon(
+                                        Icons.library_books,
+                                        color: Colors.red,
+                                        size: 20,
+                                      ),
+                                    ),
+                                  ),
+                                )
+                            ],
+                          ),
                         ),
                       );
                     }
@@ -188,7 +207,7 @@ class _PassagePageState extends State<PassagePage> {
           ]),
       // floatingActionButton: FloatingActionButton(
       //   onPressed: () {
-      //     scroolToVerse();
+      //     scroolToIndex();
       //   },
       // ),
     );
